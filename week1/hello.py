8072c9bd0a7dc6e623617e63a8d767131571d37c
Ato Nyarko <116370800+atofoh@users.noreply.github.com>
2022-10-24 23:38:03 +0000

    add hello.py

--- week1/hello.py
+++ week1/hello.py
@@ -1,1 +1,11 @@
-print("Hello,world")
+# Ask user for their name
+name = input("What is your name? ")
+
+# Remove leading and trailing whitespace
+name = name.strip()
+
+# Capitalize the first letter of the name
+name = name.title()
+
+# Say hello to the user
+print(f"hello, {name}")
